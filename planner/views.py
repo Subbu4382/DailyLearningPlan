d02c8e8488e87e61293412b0686eb3c27635717f
Subbu4382 <subrahmanyamdunne@gmail.com>
2025-12-01 17:47:15 +0530

    Daily Learning Plan

--- planner/views.py
+++ planner/views.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import Goal, DailyPlan
 from .serializers import GoalSerializer, DailyPlanSerializer
