42c5a7da0535bdcd0f185ff6d47c3ae4c259d487
Subbu4382 <subrahmanyamdunne@gmail.com>
2025-12-01 17:42:54 +0530

    Daily Learning Plan

--- planner/urls.py
+++ planner/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     # Goals
     path('goals/', GoalListCreateView.as_view(), name='goals'),
     path('goals/<int:pk>/', GoalDetailView.as_view(), name='goal-detail'),
-    path("ai/generate-plan/<int:goal_id>/", ai_generate_plan),
+ 
 
 
     # Daily Plans
@@ -20,7 +20,8 @@ urlpatterns = [
     path('daily-plans/<int:pk>/', DailyPlanDetailView.as_view(), name='dailyplan-detail'),
 
     # AI Generator
-    path('ai/generate-plan/', ai_generate_plan, name='ai-generate-plan'),
+    # path('ai/generate-plan/', ai_generate_plan, name='ai-generate-plan'),
+    path("ai/generate-plan/<int:goal_id>/", ai_generate_plan),
     path('register/', views.register_user, name='register'),
     path('login/', views.login_user, name='login'),
 ]
